2164541aafa683a09b3891570d2b5b71650c0f8f
dragon-andy <140835982+dragon-andy@users.noreply.github.com>
2023-08-08 13:28:36 +0700

    Update nontls.sh

--- websocket/nontls.sh
+++ websocket/nontls.sh
@@ -16,12 +16,12 @@ red='\e[1;31m'
 green='\e[0;32m'
 yell='\e[1;33m'
 NC='\e[0m'
-echo "Installing SSH Websocket None TLS by RstoreVPN " | lolcat
+echo "Installing SSH Websocket None TLS by KlmpkVPN " | lolcat
 echo "Progress..." | lolcat
 sleep 3
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/myridwan/VVIP/ipuk/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/dragon-andy/VVIP/ipuk/websocket/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
