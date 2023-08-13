45ae37ecce0b9676996c681f68f18a242c80a8d6
dragon-andy <140835982+dragon-andy@users.noreply.github.com>
2023-08-13 13:37:56 +0700

    Create lock.sh

---
+++ lock.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\e[32mloading...\e[0m"
+clear
+echo " "
+cat /root/log-limit.txt " username
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+# proses mengganti passwordnya
+passwd -l $username
+clear
+  echo " "
+  echo " "
+  echo " "
+  echo "-----------------------------------------------"
+  echo -e "Username ${blue}$username${NC} successfully ${red}LOCKED!${NC}."
+  echo -e "Access Login to username ${blue}$username${NC} has been locked."
+  echo "-----------------------------------------------"
+else
+echo "Username not found on your server."
+    exit 1
+fi
