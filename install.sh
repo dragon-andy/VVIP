e729c512fcb44a5903d100e347897b301d3ea842
dragon-andy <140835982+dragon-andy@users.noreply.github.com>
2023-08-13 13:03:54 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -252,7 +252,7 @@ TEXT="Project VPN TUNNELING
 <code>IP Vps     :</code> <code>$MYIP</code>
 <code>User Login :</code> <code>root</code>
 <code>Pass Login :</code> <code>bok</code>
-<code>User Script:</code> <code>$IZIN</code>
+<code>User Script:</code> <code>$Name</code>
 <code>Exp Script :</code> <code>$exp2</code>
 <code>Location   :</code> <code>$CITY</code>
 <code>Timezone   :</code> <code>$TIME</code>
